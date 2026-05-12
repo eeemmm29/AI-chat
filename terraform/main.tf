45fa91e947c4a699e578b9498238c7c9073a48de
Emir <148708322+eeemmm29@users.noreply.github.com>
2026-05-12 15:40:07 +0600

    infra: fix terraform quota error by overriding user project and billing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,9 @@
 provider "google" {
   project = var.project_id
   region  = var.region
+
+  user_project_override = true
+  billing_project       = var.project_id
 }
 
 # Identity Platform (Firebase Auth)
