37264c4309b928374ebe19eceab8f8fed95bc97f
Emir <148708322+eeemmm29@users.noreply.github.com>
2026-05-12 13:34:43 +0600

    infra: add minimal terraform config for gcp and identity platform

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_project_service" "identityplatform" {
+  service = "identitytoolkit.googleapis.com"
+}
+
+resource "google_project_service" "cloudrun" {
+  service = "run.googleapis.com"
+}
+
+resource "google_identity_platform_config" "default" {
+  project = var.project_id
+  depends_on = [google_project_service.identityplatform]
+}
